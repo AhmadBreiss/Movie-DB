7631f4535a83ddc4398398d196dbbcad0abb9320
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-31 16:39:53 +0200

    step 5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // step2
 const express = require('express');
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+];
+
 const PORT=3000;
 
 const app = express();
@@ -27,5 +34,23 @@ app.get('/search', (req, res)=>{
         res.status(500).send({status:500, error:true, message:"you have to provide a search"})
         }
 });
+// step5
+
+app.get('/movies/add', (req, res) =>{
+    res.send("movie add");
+});
+
+app.get ('/movies/get', (req, res)=>{
+    res.status(200).send({status: 200, data:movies })
+}); 
+
+app.get('/movies/edit', (req, res)=>{
+    res.send("movie edit")
+})
+
+app.get('/movies/delete', (req, res)=>{
+    res.send("movie delete");
+})
+
 
 app.listen(PORT, () => console.log(`server in now listening on port ${PORT}`))
